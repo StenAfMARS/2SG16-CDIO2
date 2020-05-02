6709adab861936a8f5a15f4d0010b9401f60d56a
adrian <skymon65@gmail.com>
2020-05-02 12:23:39 +0200

    Made IUserDTO and created UserDTO for refrence purposes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in, "CP850");
+        System.out.println(in.nextLine());
+    }
+
 }
