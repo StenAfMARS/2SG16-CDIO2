e5e2cafceda4d4cc7a5f6702799aa9148b028080
adrian <skymon65@gmail.com>
2020-05-09 21:25:58 +0200

    Rest set up with a @webServlet httpServlet

--- src/main/java/Appconfig.java
+++ src/main/java/Appconfig.java
@@ -1,7 +1,56 @@
+import dto.UserPassDTO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@ApplicationPath("/rest")
-public class Appconfig extends Application {
+@WebServlet(
+        name = "rest API",
+        urlPatterns = "/rest/*",
+        loadOnStartup = 1)
+public class Appconfig extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String[] uri = req.getPathInfo().split("/");
+        PrintWriter writer = resp.getWriter();
+
+        if (uri.length == 2)
+        switch (uri[1]){
+            case "login":
+                writer.println(
+                    LoginManager.auth(
+                        req.getParameter("username"),
+                        req.getParameter("password"))
+                );
+                break;
+        }
+
+        writer.close();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
 }
