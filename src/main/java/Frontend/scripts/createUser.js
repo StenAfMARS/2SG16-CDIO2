b952b49f7198b908d4046313d12ab53beebb65d2
mathias <mathias.baerentzen@gmail.com>
2020-05-09 12:00:28 +0200

    add create user

---
+++ src/main/java/Frontend/scripts/createUser.js
@@ -0,0 +1,21 @@
+function createIngredient() {
+    event.preventDefault();
+    var data = $('#ingredientform').serializeJSON();
+    $.ajax({
+        url: 'rest/User',
+        method: 'POST',
+
+        contentType: "application/json", // det visender er json
+        data: data,
+        success: function (data) {
+            alert(JSON.stringify(data));
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.responseText);
+            alert(textStatus);
+            alert(errorThrown);
+
+        }
+    });
+}
